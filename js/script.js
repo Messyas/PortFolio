34830d1e946fd871ded0fcb0185d15904d97c30c
Messyas <messyasgoisfranca@gmail.com>
2024-01-11 09:51:55 -0400

    Pequenas modificacoes

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
  /* typed js */
 
  const typed = new Typed('.multiple-text', {
-  strings: ['FullStack Developer', 'Designer', 'data scientist'],
+  strings: ['Desenvolvedor FullStack', 'Analista de Sistemas', 'Cientista de Dados'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay:1000,
